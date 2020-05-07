1091511dc0c8e6f106afb7751f5957d0aa902fa0
Robin Kataria <robin8kataria@gmail.com>
2020-05-07 13:15:02 +0530

    Add Header and navigation links

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 
 import Homepage from "./pages/Homepage/Homepage.component";
 import ShopPage from "./pages/ShopPage/ShopPage.component";
+import Header from "./components/Header/Header.component";
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path="/" component={Homepage} />
                 <Route path="/shop" component={ShopPage} />
---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/hat.svg";
+
+import "./Header.styles.scss";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <Link to="/">
+                    <Logo className="logo" />
+                </Link>
+            </div>
+            <div className="options">
+                <Link className="option" to="/shop">
+                    SHOP
+                </Link>
+                <Link className="option" to="/contact">
+                    CONTACT
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
